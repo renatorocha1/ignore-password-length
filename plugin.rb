0cd8b0afe9c19a8e34b61cf0315dbf910927996d
Renato Rocha <hi@renatorocha.net>
2024-06-16 17:54:18 +1200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,13 +5,30 @@
 # authors: Renato Rocha
 # url: https://github.com/renatorocha1/ignore_password_length
 
-after_initialize do
-    # Unregister the existing password length validation
-    User.unregister_validator("password")
+enabled_site_setting :ignore_password_length_enabled
 
-    # Register a custom password validator that does nothing
-    User.register_validator("password") do
-      # Skip password length validation
-      true
+after_initialize do
+  # Custom password validator that does nothing
+  module ::IgnorePasswordLength
+    class Engine < ::Rails::Engine
+      engine_name "ignore_password_length"
+      isolate_namespace IgnorePasswordLength
+    end
+
+    def self.skip_password_length_validation
+      User.class_eval do
+        validate :custom_password_validation
+
+        def custom_password_validation
+          # Skip password length validation
+          return true
+        end
+      end
     end
   end
+
+  # Hook into the Rails initialization process to apply our custom validator
+  Rails.configuration.to_prepare do
+    IgnorePasswordLength.skip_password_length_validation
+  end
+end
